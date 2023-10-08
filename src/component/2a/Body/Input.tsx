b0be0ee64e7c38ba761bd09810417583144140f3
Nguyen Huynh Huong <huynhhuong205k@gmail.com>
2023-10-08 19:52:39 +0700

    useState

--- src/component/2a/Body/Input.tsx
+++ src/component/2a/Body/Input.tsx
@@ -1,22 +1,48 @@
-import React from "react";
-import { View, TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Text, TouchableOpacity } from "react-native";
 import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 import FontistoIcon from "react-native-vector-icons/Fontisto";
-import Entypo from 'react-native-vector-icons/Entypo'
+import Entypo from 'react-native-vector-icons/Entypo';
 import { styles } from "./style";
 
 const Input = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.input1}>
-        <FontAwesomeIcon name="user" size={30}  />
-        <TextInput placeholder="user name" style={styles.input} />
+        <FontAwesomeIcon name="user" size={30} />
+        <TextInput
+          placeholder="user name"
+          style={styles.input}
+          value={userName}
+          onChangeText={(text) => setUserName(text)}
+        />
       </View>
       <View style={styles.input2}>
-        <FontistoIcon name="locked" size={30}  />
-        <TextInput placeholder="password" style={styles.input} />
-        <Entypo name="eye"  size={30} style={styles.icon}/>
+        <FontistoIcon name="locked" size={30} />
+        <TextInput
+          placeholder="password"
+          style={styles.input}
+          secureTextEntry={!showPassword}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Entypo
+          name={showPassword ? "eye" : "eye-with-line"}
+          size={30}
+          style={styles.icon}
+          onPress={toggleShowPassword}
+        />
       </View>
+      <TouchableOpacity onPress={() => alert(`UserName: ${userName}, Password: ${password}`)}>
+      </TouchableOpacity>
     </View>
   );
 };
